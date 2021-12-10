7ae00f2451a910c9652559b5854b531aad5746f7
ashtonhess <ashton.hess@yahoo.com>
2021-12-10 14:25:06 -0600

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Ashton Hess on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
